98fe5dbbc1162b46134d9e0a9e034c0851299e8c
Simen Ihlen <simenihlen@gmail.com>
2022-10-06 17:46:40 +0200

    Upload via VS Code

---
+++ .history/Hide-reddit-premium-ad_20221006174606.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Hide-reddit-premium-ad
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Simen HI
+// @match        *://*.reddit.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+window.addEventListener("load", function() {
+    document.querySelectorAll("[href='/premium']")
+        .forEach(i => (i.style.display = 'none'));
+}); 
