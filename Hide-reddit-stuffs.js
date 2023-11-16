6a48e837b02b567e18091c6689b80cd2b77a10df
Simen Ihlen <simenihlen@gmail.com>
2023-11-16 16:39:28 +0100

    Userscript oppdatering

--- Hide-reddit-stuffs.js
+++ Hide-reddit-stuffs.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Hide-reddit-premium-ad
+// @name         Hide-reddit-stuffs
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
